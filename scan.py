48a5b509a567769e83a89ee1f36ae0162bf4eee0
Rithvik Pudota <pudotr@amazon.com>
2020-07-30 18:07:04 -0700

    support network speed tests and device vendor queries

--- scan.py
+++ scan.py
@@ -1,8 +1,10 @@
 import dataclasses
+import speedtest
 import time
 import socket
 import boto3
 import os
+import external
 
 from typing import Optional, List
 from scapy.all import arping, ARP, Ether
@@ -13,6 +15,7 @@ class Device:
     mac_address: str
     ip_address: str
     hostname: str
+    vendor: str
 
 @dataclasses.dataclass
 class Scan:
@@ -20,10 +23,7 @@ class Scan:
     user: str
     network_id: str
     timestamp: float
-
-# bandwidth of each device
-# network speed test
-# check internet bandwidth
+    speed_test: dict
 
 def __scan_network(network_id: str, verbose: bool) -> List[Device]:
     scan_data: List[Device] = []
@@ -35,9 +35,20 @@ def __scan_network(network_id: str, verbose: bool) -> List[Device]:
         ip_address = s[ARP].pdst
         hostname = socket.getfqdn(ip_address)
 
-        scan_data.append(Device(mac_address, ip_address, hostname))
+        scan_data.append(Device(mac_address, ip_address, hostname, external.get_mac_address_vendor(mac_address)))
 
     return scan_data
+
+def __speed_test_network() -> dict:
+    speedTest = speedtest.Speedtest()
+    speedTest.get_best_server()
+
+    speedTest.download()
+    speedTest.upload()
+
+    speedTest.results.share()
+
+    return speedTest.results.dict()
 
 def __save_scan_data_to_dynamo_db(scan: Scan):
     dynamodb = boto3.resource('dynamodb')
@@ -47,15 +58,27 @@ def __save_scan_data_to_dynamo_db(scan: Scan):
         Item={
             'id': scan.user + '-' + scan.network_id,
             'timestamp': int(scan.timestamp),
+            # TODO: bandwidth of each device
             'devices': [{
                 'mac_address': d.mac_address,
                 'ip_address': d.ip_address,
-                'hostname': d.hostname
-            } for d in scan.devices]
+                'hostname': d.hostname,
+                'vendor': d.vendor
+            } for d in scan.devices],
+            # network speed test
+            'speed_test': {
+                'download': str(scan.speed_test['download']),
+                'upload': str(scan.speed_test['upload']),
+                'ping': str(scan.speed_test['ping']),
+                'bytes_sent': scan.speed_test['bytes_sent'],
+                'bytes_received': scan.speed_test['bytes_received'],
+                'share': scan.speed_test['share'],
+                'isp': scan.speed_test['client']['isp'],
+                'isprating': scan.speed_test['client']['isprating'],
+                'country': scan.speed_test['client']['country'],
+            }
         }
     )
-
-    print(response)
 
     return response
 
@@ -69,12 +92,19 @@ def scan_network_repeatedly(network_id: str, delay: int, amount: Optional[int], 
 
     while True:
         scan_data = __scan_network(network_id, verbose)
-        scan = Scan(scan_data, os.environ['USER'], network_id, time.time())
+        speed_test_data = __speed_test_network()
+        scan = Scan(scan_data, os.environ['USER'], network_id, time.time(), speed_test_data)
 
-        __save_scan_data_to_dynamo_db(scan, )
+        __save_scan_data_to_dynamo_db(scan)
+        scan_count += 1
 
-        scan_data_list = map(lambda d: [d.mac_address, d.ip_address, d.hostname], scan_data)
-        print(tabulate(scan_data_list, headers=['Mac Address', 'IP Address', 'Hostname']))
+        scan_data_list = map(lambda d: [d.mac_address, d.ip_address, d.hostname, d.vendor], scan_data)
+        print(tabulate(scan_data_list,
+            headers=['Mac Address', 'IP Address', 'Hostname', 'Vendor']))
+        print()
+
+        print(tabulate([[speed_test_data['download'], speed_test_data['upload'], speed_test_data['ping']]],
+            headers=['Download', 'Upload', 'Ping']))
         print()
 
         if amount is None or scan_count < amount:
@@ -83,4 +113,4 @@ def scan_network_repeatedly(network_id: str, delay: int, amount: Optional[int], 
             break
 
 if __name__ == "__main__":
-    scan_network_repeatedly("192.168.1.0/24", 300, 5)
+    scan_network_repeatedly("192.168.1.0/24", 10, 5)
